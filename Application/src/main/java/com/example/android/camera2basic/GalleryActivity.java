b4cefcd2bf5cf58c238eb57e58e11801a6408ff8
Victor Ruch <viruch3112@gmail.com>
2017-09-28 14:55:52 +0200

    adding SettingsActivity

--- Application/src/main/java/com/example/android/camera2basic/GalleryActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/GalleryActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.camera2basic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.camera2basic.adapters.MyAdapter;
 import com.example.android.camera2basic.data.Face;
@@ -42,6 +45,25 @@ public class GalleryActivity extends AppCompatActivity {
         List<Face> imageItems = prepareData();
         MyAdapter adapter = new MyAdapter(getApplicationContext(), imageItems);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     private List<Face> prepareData() {
